4407a0f5b30ef4ee9c32e399a5615b1f4b18d4d9
avmochalov <al.mochalov93@gmail.com>
2023-11-21 18:26:43 +0300

    select_training

---
+++ src/pages/Profile/NewLogin.jsx
@@ -0,0 +1,17 @@
+import S from "./NewLogin.module.css";
+import logo from "../../imgAl/logo.png";
+
+function NewLogin() {
+  return (
+    <div className={S.newpwd_window}>
+      <img className={S.newpwd_logo} src={logo} alt="logo" />
+      <form action="" className={S.newpwd_form}>
+        <p className={S.form_header}>Новый логин:</p>
+        <input type="text" placeholder="Введите логин" className={S.newpwd_input}/>
+        <button type="submit" className={S.form_button}>Сохранить</button>
+      </form>
+    </div>
+  );
+}
+
+export default NewLogin;
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,10 +1,16 @@
 /* eslint-disable import/named */
+import { useState } from "react";
 import S from "./Profile.module.css";
 import logo from "../../imgAl/logo.png";
 import arrow from "../../imgAl/arrow.png";
-import  NewPwd  from "./NewPwd";
+import NewPwd from "./NewPwd";
+import NewLogin from "./NewLogin";
+import SelectWorkout from "./SelectWorkout";
 
 function Profile() {
+  const [isNpwOpen, setIsNpwOpen] = useState(false);
+  const [isNlogOpen, setIsNlogOpen] = useState(false);
+
   return (
     <div className={S.profile_page}>
       <header className={S.header}>
@@ -14,18 +20,28 @@ function Profile() {
           <div className={S.user_block__name}>Сергей</div>
           <img className={S.user_block__arrow} src={arrow} alt="arrow" />
         </div>
-        <NewPwd />
+        {/* <SelectWorkout /> */}
+        {isNpwOpen && <NewPwd />}
+        {isNlogOpen && <NewLogin />}
       </header>
       <div className={S.profile_block}>
         <p className={S.profile_block__header}>Мой профиль</p>
         <p className={S.profile_block__data}>Логин: sergey.petrov96</p>
         <p className={S.profile_block__data}>Пароль: 4fkhdj880d</p>
         <div className={S.button_block}>
-          <button className={S.profile_block__button} type="button">
+          <button
+            className={S.profile_block__button}
+            type="button"
+            onClick={() => setIsNlogOpen(!isNlogOpen)}
+          >
             Редактировать логин
           </button>
-          <button className={S.profile_block__button} type="button">
-            Редактировать логин
+          <button
+            className={S.profile_block__button}
+            type="button"
+            onClick={() => setIsNpwOpen(!isNpwOpen)}
+          >
+            Редактировать пароль
           </button>
         </div>
         <div className={S.mycourses_block}>
