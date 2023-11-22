409f8ef4beab22173352137a093ac6ca27c15a69
Regina Askerova <saparova.regina@yandex.ru>
2023-11-23 02:15:53 +0300

    login_register_screens

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,34 @@
-import S from "./Login.module.css";
+import { NavLink } from "react-router-dom";
+import styles from "./Login.module.css";
 
 function Login() {
   return (
-    <div className={S.test}>
-      <h1 className={S.test__header}>Это тестовая страница компонента Login</h1>
+    <div className={styles.body__login}>
+      <section className={styles.login__screen}>
+        <div className={styles.login__wrapper}>
+          <NavLink to="/">
+            <div className={styles.login__logo} />
+          </NavLink>
+          <input
+            type="text"
+            className={styles.login__name}
+            placeholder="Логин"
+          />
+          <input
+            type="text"
+            className={styles.login__password}
+            placeholder="Пароль"
+          />
+          <button type="button" className={styles.login__button}>
+            Войти
+          </button>
+          <NavLink to="/register">
+            <button type="button" className={styles.register__button}>
+              Зарегистрироваться
+            </button>
+          </NavLink>
+        </div>
+      </section>
     </div>
   );
 }
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,11 +1,31 @@
+import { NavLink } from "react-router-dom";
 import S from "./Register.module.css";
 
 function Register() {
   return (
-    <div className={S.test}>
-      <h1 className={S.test__header}>
-        Это тестовая страница компонента Register
-      </h1>
+    <div className={S.body__register}>
+      <section className={S.register__screen}>
+        <div className={S.register__wrapper}>
+          <NavLink to="/">
+            <div className={S.login__logo} />
+          </NavLink>
+          <input type="text" className={S.register__name} placeholder="Логин" />
+          <input
+            type="text"
+            className={S.register__password}
+            placeholder="Пароль"
+          />
+          <input
+            type="text"
+            className={S.repeat__password}
+            placeholder="Повторите пароль"
+          />
+
+          <button type="button" className={S.register__button}>
+            Зарегистрироваться
+          </button>
+        </div>
+      </section>
     </div>
   );
 }
