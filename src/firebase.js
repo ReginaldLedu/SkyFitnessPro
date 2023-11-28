3e84acc411bfb43b75c69be9e8f1d86868207427
AkoostA <123000618+AkoostA@users.noreply.github.com>
2023-11-28 15:29:06 +0300

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDFKTcYkmj-xGylLzx49qhQUOZzkjttUEg",
+  authDomain: "skyfitnesspro-abf64.firebaseapp.com",
+  databaseURL:
+    "https://skyfitnesspro-abf64-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "skyfitnesspro-abf64",
+  storageBucket: "skyfitnesspro-abf64.appspot.com",
+  messagingSenderId: "901940111312",
+  appId: "1:901940111312:web:d39bd7a93fa64f153f7bd5",
+};
+
+const app = initializeApp(firebaseConfig);
